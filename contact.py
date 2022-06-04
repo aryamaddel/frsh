38d9ca60af2619b860b9dadae3564495c171d227
Arya Maddel <maddelarya@gmail.com>
2022-06-04 17:32:25 +0530

    forms and user and input#4

--- contact.py
+++ contact.py
@@ -7,4 +7,4 @@ from wtforms.validators import DataRequired, Length, Email
 class contact_from(FlaskForm):
     email = StringField('Email address', validators=[DataRequired(), Email()])
     message = StringField('Message for us')
-    submit = SubmitField('submit')
+    submit = SubmitField('Send')
