65da47959786f14f320ea6f5b29b1fc5616af131
RuchirKalokhe <ruchirkalokhe@gmail.com>
2025-01-23 20:04:00 +0530

    Ruchir info added

--- pages/about.vue
+++ pages/about.vue
@@ -23,8 +23,7 @@
                 <h2 class="text-lg font-semibold mb-2 text-gray-800">Ruchir Kalohke</h2>
                 <nuxt-img src="team/Ruchir.jpg" alt="Ruchir Kalohke" class="mb-3 rounded shadow-lg w-full max-w-xs mx-auto" />
                 <p class="text-gray-600 leading-relaxed">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-                    Sed cursus ante dapibus diam.
+                    Pata nahi bhai kuch raha hu...
                 </p>
             </section>
         </div>
