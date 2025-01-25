37529eccda6a9f72434cebc299e811c6335fbe9b
aryamaddel <aryamaddel@gmail.com>
2025-01-25 15:13:51 +0530

    Add Nuxt Content module and create initial blog structure

---
+++ content/blog/first.md
@@ -0,0 +1,8 @@
+---
+title: First
+description: My first blog post
+---
+
+# First
+
+This is my first blog post.
---
+++ pages/[post].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data: blogPost } = await useAsyncData(route.path, () => {
+  return queryCollection('content').path(`/blog${route.path}`).first()
+})
+</script>
+
+<template>
+  <ContentRenderer v-if="blogPost" :value="blogPost" />
+  <div v-else>Post not found</div>
+</template>
