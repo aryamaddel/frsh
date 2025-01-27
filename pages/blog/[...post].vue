c01a22685709cb5985dc48522db5111f4652c264
aryamaddel <aryamaddel@gmail.com>
2025-01-27 20:59:47 +0530

    Rename blog post data variable for consistency in the blog post component

--- pages/blog/[...post].vue
+++ pages/blog/[...post].vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 const route = useRoute()
-const { data: blogPost } = await useAsyncData(() => { return queryCollection('blog').path(route.path).first() })
+const { data: post } = await useAsyncData(() => { return queryCollection('blog').path(route.path).first() })
 </script>
 
 <template>
   <NuxtLink to="/blog">Back to blog List</NuxtLink>
-  <ContentRenderer v-if="blogPost" :value="blogPost" class="prose" />
+  <ContentRenderer v-if="post" :value="post" class="prose" />
   <div v-else>Post not found</div>
 </template>
