9b6039b469781fc77fe98bf4c0d1b951bf0a1e78
aryamaddel <aryamaddel@gmail.com>
2025-04-01 18:30:47 +0530

    Update compatibility date in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/content"],
-  compatibilityDate: "2025-01-29",
+  compatibilityDate: "2025-04-01",
 });
